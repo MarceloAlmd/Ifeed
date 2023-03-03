f96a2062958425e61e043ce7c7dbd10f3c902f9b
Marcelo <marcelo3x4@hotmail.com>
2023-03-03 11:06:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {Post} from './post'
+export function App() {
+  return (
+    <Post 
+      author="Marcelo Magalhães"
+      content="lorem ipsum dolor sit amet consectutet"
+    />
+  )
+}
+
+
